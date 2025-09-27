2a638a79de29d662c698c567aef3894d1f9721ab
unknown <iveta.zalite@gmail.com>
2025-09-27 23:56:01 +0300

    new example

---
+++ age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    char name[50];
+    
+    printf("Enter your name: ");
+    scanf("%s", name);
+    
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    printf("Hello %s, you are %d years old!\n", name, age);
+    return 0;
+}
